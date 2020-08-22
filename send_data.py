b85a8d0e67594d980378e8a015058e5e19fdbcdb
SamSaraSelva <47019583+SamSaraSelva@users.noreply.github.com>
2020-08-22 16:09:36 +0800

    Update send_data.py

--- send_data.py
+++ send_data.py
@@ -1,43 +1,43 @@
 import random  
 import time  
- 
+  
 from azure.iot.device import IoTHubDeviceClient, Message  
- 
-CONNECTION_STRING = "HostName=SmartFarming.azure-devices.net;DeviceId=mypi;SharedAccessKey=WU7LtDbvm0zxHt6az2NYEQxBv5Et1ggHbg0XxL55Zds="  
- 
+  
+CONNECTION_STRING = "Connection_String Here"  
+  
 TEMPERATURE = 20.0  
 HUMIDITY = 60  
 MSG_TXT = '{{"temperature": {temperature},"humidity": {humidity}}}'  
- 
+  
 def iothub_client_init():  
-    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)  
+    client = IoTHubDeviceClient.create_from_connection_string(HostName=SmartFarming.azure-devices.net;DeviceId=mypi;SharedAccessKey=WU7LtDbvm0zxHt6az2NYEQxBv5Et1ggHbg0XxL55Zds=)  
     return client  
- 
+  
 def iothub_client_telemetry_sample_run():  
- 
+  
     try:  
         client = iothub_client_init()  
         print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )  
         while True:  
- 
+            
             temperature = TEMPERATURE + (random.random() * 15)  
             humidity = HUMIDITY + (random.random() * 20)  
             msg_txt_formatted = MSG_TXT.format(temperature=temperature, humidity=humidity)  
             message = Message(msg_txt_formatted)  
- 
+  
             if temperature > 30:  
               message.custom_properties["temperatureAlert"] = "true"  
             else:  
               message.custom_properties["temperatureAlert"] = "false"  
- 
+  
             print( "Sending message: {}".format(message) )  
             client.send_message(message)  
             print ( "Message successfully sent" )  
             time.sleep(3)  
- 
+  
     except KeyboardInterrupt:  
         print ( "IoTHubClient sample stopped" )  
- 
+  
 if __name__ == '__main__':  
     print ( "IoT Hub Quickstart #1 - Simulated device" )  
     print ( "Press Ctrl-C to exit" )  
